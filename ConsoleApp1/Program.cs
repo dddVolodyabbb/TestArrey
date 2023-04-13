fa42ccb0927db67077773a53cd3b1c02cdc37c69
ddd_Volodya_bbb <volodya.logno@yandex.ru>
2023-04-13 18:28:07 +0400

    Create project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]=random.Next(0,11);
+            }
+
+            foreach (var i in arr)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+            Array.Sort(arr);
+            foreach (var i in arr)
+            {
+                Console.Write(i+ " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
