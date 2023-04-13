bfee12557ffb251dff62db5686b0b59d20d6f40c
ddd_Volodya_bbb <volodya.logno@yandex.ru>
2023-04-13 18:59:44 +0400

    Push

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,14 @@ namespace ConsoleApp1
     internal class Program
     {
         private const int nomberCount = 20;
+        private const int nomberStart = 0;
+        private const int nomberEnd= 99;
         private static Random rnd = new Random();
         static void Main(string[] args)
         {
-            var arrey = Enumerable.Range(0, nomberCount).Select(_ => rnd.Next(10)).ToArray();
+            var arrey = Enumerable.Range(0, nomberCount)
+                .Select(_ => rnd.Next(nomberStart, nomberEnd+1))
+                .ToArray();
             arrey = arrey.OrderBy(x=>x).ToArray();
             Console.Write(string.Join(", ", arrey));
             Console.ReadKey();
